64447dc4e136ecd1a15a2796060f4497372fc941
Denchik_1_1 <denistronin30@gmail.com>
2020-11-23 16:55:41 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('.slider').slick({
+    dots:true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed:700,
+    prevArrow: ('.prev'),
+    nextArrow: ('.next'),
+});
+$('.single-item').slick('slickPlay');
+
